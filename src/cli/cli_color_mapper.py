aa6537b79cd147e0d883cc262c5bede8f6983027
hfessler <49552433+aiventures@users.noreply.github.com>
2024-09-20 23:49:38 +0200

    Creating Color Themes

--- src/cli/cli_color_mapper.py
+++ src/cli/cli_color_mapper.py
@@ -1,13 +1,19 @@
 """ Color Mapping.
+    More on styles and theme manaager:
+    Styles https://rich.readthedocs.io/en/stable/style.html
+    Theme Manager https://pypi.org/project/rich-theme-manager/
 """
 
 import logging
 import os
 from pathlib import Path
+from enum import Enum
 from typing import Any
 from rich.table import Table
 from rich.console import Console
 from rich.logging import RichHandler
+from rich.style import Style
+from rich_theme_manager import Theme, ThemeManager
 
 # TODO MOVE THIS TO A CONFIG FILE
 from util.const_local import LOG_LEVEL
@@ -21,14 +27,39 @@ NAME = "name" # as in ANSI COLOR NAMES
 RGB = "rgb"
 THEME = "theme"
 THEMES = "themes"
+DEFAULT = "default"
+THEME_DEFAULT="ubuntu"
 COLORS = "colors"
+STYLES = "styles"
+COLOR = "color"
+TAGS = "tags"
+STYLE_DESCRIPTION = "description"
+P_RESOURCES = "resources"
+P_RICH_THEMES = "rich_themes"
+F_COLOR_THEMES = "color_themes.json"
+F_STYLES = "styles.json"
 BASE_HEX_COLORS = HEX_COLORS[:16]
 BASE_RGB_COLORS = RGB_COLORS[:16]
+
+class RichStyle(Enum):
+    """ Rich Style Enum """
+    COLOR = "color"
+    BG_COLOR = "bgcolor"
+    BOLD = "bold"
+    UNDERLINE = "underline"
+    ITALIC = "italic"
+    REVERSE = "reverse"
+    STRIKE = "strike"
+
+    @staticmethod
+    def values():
+        """ returns list of values defined in Enum """
+        return list(map(lambda c: c.value, RichStyle))
 
 logger = logging.getLogger(__name__)
 
 # switch to 256 Colors as default
-console = Console(color_system="256")
+# console = Console(color_system="256")
 
 class ColorMapper():
     """ Class to handle Color Mappings """
@@ -36,7 +67,11 @@ class ColorMapper():
     def _read_themes(self)->dict:
         """ reads themes for standard colors, returns theme dictionary """
         out = {}
-        _f_themes = os.path.join(Path(__file__).parent,"themes.json")
+        # TODO ALLOW TO CHANGE PATH in Constructor
+        # self._f_color_themes = None
+        _f_themes = os.path.join(self._f_color_themes)
+
+        # _f_themes = os.path.join(Path(__file__).parent.parent.parent,"resources",F_COLOR_THEMES)
         _themes = Persistence.read_json(_f_themes)
         for _theme in _themes:
             _name = _theme.get(NAME)
@@ -56,8 +91,16 @@ class ColorMapper():
             out.append(group_list[_from:_to])
         return out
 
-    def show_colors(self,num_colums:int=8,colors:list=[])->None:
+    def show_colors(self,num_colums:int=8,colors:list=None,console:Console=None)->None:
         """ displays all colors, you also my use filter strings """
+        if colors is None:
+            colors = []
+
+        if console:
+            _console = console
+        else:
+            _console = Console(color_system="256")
+
         _table = Table(title=f"Color Codes matching colors {colors}")
         _color_list_config = self.config["colors"]
         _keys = []
@@ -79,10 +122,10 @@ class ColorMapper():
                         passed = True
             if passed is False:
                 continue
-            
+
             s = f"[white on {_color[HEX]}]{str(_color[CODE]).zfill(3)} {_color[NAME]: <15}"
             _color_list.append(s)
-        # get a sorted list of items        
+        # get a sorted list of items
         _keys = []
         for v in _color_sort.values():
             _keys.extend(v)
@@ -90,14 +133,19 @@ class ColorMapper():
         rows = self.group_list(_keys,num_colums)
         for row in rows:
             _table.add_row(*row)
-        console.print(_table)
+        _console.print(_table)
 
-    def show_themes(self,num_columns:int=9)->None:
+    def show_themes(self,num_columns:int=9,console:Console=None)->None:
         """ displays all standard color themes grouped into tables """
         _standard_colors = COLOR_NAMES[:16]
         _themes = self._read_themes()
         _all_themes = list(_themes.keys())
         _all_themes = self.group_list(_all_themes,num_columns)
+        if console:
+            _console = console
+        else:
+            _console = Console(color_system="256")
+
 
         for _themes_out in _all_themes:
             _table = Table(title=f"THEMES {_themes_out}")
@@ -111,7 +159,7 @@ class ColorMapper():
                     _hex = _theme[_c]
                     row.append(f"[{_hex}]{_c}")
                 _table.add_row(*row)
-            console.print(_table)
+            _console.print(_table)
 
     def code2rgb(self,code:int)->str:
         """ convert code to rgb  """
@@ -198,7 +246,11 @@ class ColorMapper():
         except ValueError:
             return None
 
-    def __init__(self,theme:str=None) -> None:
+    def __init__(self,theme:str=None,p_resources:str=None) -> None:
+        """ constructor
+            theme: theme to set (defined in color_themes.json)
+            p_resources: Configuration Path (defaults to /resources)
+        """
         self._code_map = {
             CODE:{RGB:self.code2rgb,NAME:self.code2name,HEX:self.code2hex},
             RGB:{NAME:self.rgb2name,HEX:self.rgb2hex,CODE:self.rgb2code},
@@ -206,12 +258,30 @@ class ColorMapper():
             HEX:{RGB:self.hex2rgb,NAME:self.hex2name,CODE:self.hex2code},
             }
 
+        # set the configuration
+        self._p_resources = p_resources
+        self._f_color_themes = None
+        self._set_config_path()
+
         self._hexcolors = HEX_COLORS
         self._rgb_colors = RGB_COLORS
         self._themes = {}
         self._theme = None
         if theme:
             self.theme = theme
+
+    def _set_config_path(self):
+        """ sets config path  """
+
+        # set a default path
+        if self._p_resources is None or not os.path.isdir(self._p_resources):
+            self._p_resources = os.path.join(Path(__file__).parent.parent.parent,"resources")
+            logger.info("[ColorMapper] Setting resource path [{self._p_resources}]")
+
+        self._f_color_themes = os.path.join(self._p_resources,F_COLOR_THEMES)
+        if not os.path.isfile(self._f_color_themes):
+            logger.warning("[ColorMapper] Couldn't find color theme file [{self._f_color_themes}]")
+            self._f_color_themes = None
 
     @property
     def config(self)->dict:
@@ -285,13 +355,210 @@ class ColorMapper():
             out.append(_rgb_col)
         return out
 
+class ThemeConsole(ColorMapper):
+    """" Handle Themes and rich Styles and create a themed console.
+        themes are names the color mapping schemas in the class above
+        style_... are the objects used in rich in conjunction with the themes
+        defined there (so we have two separate things here)
+    """
+
+    def __init__(self, theme: str = THEME_DEFAULT,color_system:str="256",create_themes:bool=False) -> None:
+        """Constructor.
+
+        Args:
+            theme (str, optional): Instanciate Console using theme.
+            color_system (str, optional): The Color System as defined by rich . Defaults to "256".
+            create_themes (bool, optional): Create the Rich Themes definition files in /resources/rich_themes.
+            Defaults to False (needs to be run at least once).
+        """
+        super().__init__(theme)
+        self._color_system = color_system
+        # self._console = Console(color_system=color_system)
+        self._p_rich_themes = None
+        self._set_rich_themes_path()
+        # self._rich_styles = {}
+        # self._read_styles()
+
+        self._theme_manager = None
+        # todo handle themes
+        if create_themes:
+            self._theme_manager = self.create_rich_themes()
+        if not self._theme_manager:
+            self._theme_manager = ThemeManager(theme_dir=self._p_rich_themes)
+        self._style_theme = None
+        self._set_style_theme(theme)
+
+    def read_styles(self)->dict:
+        """ reads the styles file is existent """
+        styles_dict = {}
+        _f_styles = os.path.join(self._p_resources,F_STYLES)
+        if os.path.isfile(_f_styles):
+            styles_dict = Persistence.read_json(_f_styles)
+            if isinstance(styles_dict,dict):
+                logger.debug(f"[ThemeConsole] Read [{len(styles_dict)}] styles from [{_f_styles}]")
+            else:
+                styles_dict = {}
+                logger.warning(f"[ThemeConsole] invalid data in styles file [{_f_styles}]")
+        else:
+            logger.warning(f"[ThemeConsole] invalid path to styles file [{_f_styles}]")
+        return styles_dict
+
+    def _set_style_theme(self,theme:str=None)->None:
+        """ setting the style theme """
+        if theme in self.themes:
+            self._style_theme = theme
+
+    def preview_theme(self,theme:str=THEME_DEFAULT)->None:
+        """ displays a theme """
+        _themes = list(self.themes)
+        try:
+            _theme = self._theme_manager.get(theme)
+            self._theme_manager.preview_theme(_theme)
+        except ValueError:
+            print(f"[ThemeConsole] No Theme named [{theme}], available {_themes} ")
+            return
+
+    def preview_themes(self)->None:
+        """ show all availabkle themes """
+        for _theme in self.themes:
+            self.preview_theme(_theme)
+
+    @property
+    def themes(self):
+        """ returns list of available themes """
+        return [t.name for t in self._theme_manager.themes]
+
+    def list_themes(self):
+        """ shows the list of themes """
+        self._theme_manager.list_themes()
+
+    def create_rich_themes(self)->ThemeManager:
+        """ parses the original thems and transforms them to rich themes if not created before
+            returns the Theme Manage
+        """
+        if not self._p_rich_themes:
+            logger.warning("[ThemeConsole] No valid path for Rich Themes, check configuration")
+            return
+
+        _themes = self._read_themes()
+        _base_colors = COLOR_NAMES[:16]
+        _rich_themes = []
+        # reading the styles from a config file
+        _rich_styles = self.read_styles()
+        _default_color = _rich_styles.get(DEFAULT,{}).get(COLOR,"white")
+        _style_list = RichStyle.values()
+
+        for _theme_name,_theme_info in _themes.items():
+            _description = f"Theme {_theme_name} generated using ThemeConsole"
+            _theme = {NAME:_theme_name,STYLE_DESCRIPTION:_description}
+            _styles = {}
+            # create the default styles for the basic colors
+            for _col_name in _base_colors:
+                _color = _themes[_theme_name].get(_col_name,_default_color)
+                _style = {}
+                _style[COLOR]=_color
+                _styles[_col_name]=Style(**_style)
+
+            # now add the custom styles and replace any default colors by
+            for _custom_style,_custom_info in _rich_styles.items():
+                _custom_style_info = {}
+                for _style in _style_list:
+                    _value  = _custom_info.get(_style)
+                    if _value is None:
+                        continue
+                    # try to get theme color otherwise use the existing one
+                    if "color" in _style:
+                        _style_col = _styles.get(_value)
+                        if _style_col:
+                            _value = _style_col.color.name
+                    _custom_style_info[_style]=_value
+                if len(_custom_info) > 0:                    
+                    _styles[_custom_style] = Style(**_custom_style_info)
+
+            _theme[STYLES]=_styles
+            _theme[TAGS]=["os_theme"]
+
+            logger.debug(f"[ThemeConsole] Creating Theme [{_theme_name}]")
+            _rich_themes.append(Theme(**_theme))
+        return ThemeManager(theme_dir=self._p_rich_themes, themes=_rich_themes,overwrite=True)
+
+    def _set_rich_themes_path(self):
+        """" setting the rich themes folder as a subfolder of the resources folder """
+        if os.path.isdir(self._p_resources):
+            _p_rich_themes = os.path.join(self._p_resources,P_RICH_THEMES)
+            if not os.path.isdir(_p_rich_themes ):
+                Path(self._p_rich_themes).mkdir(parents=True)
+            self._p_rich_themes = _p_rich_themes
+        else:
+            logger.warning("f[ThemeConsole] No valid resources path [{self._p_resources}], check your configuration")
+
+
+# def test_theme():
+#     k = {"color":"#126608","bold":False}
+#     THEMES = [
+#         Theme(
+#             name="dark",
+#             description="Dark mode theme",
+#             tags=["dark"],
+#             styles={
+#                 # "info": Style(color="#126608",bold=True,reverse=False,link="hugo"),
+#                 # "info": Style(color="#126608",bold=True,reverse=False,frame=True),
+#                 "info": Style(**k),
+#                 "yellow":"yellow2",
+#                 "warning": "bold magenta",
+#                 "danger": "bold red",
+
+#         ),
+#         Theme(
+#             name="mono",
+#             description="Monochromatic theme",
+#             tags=["mono", "colorblind"],
+#             styles={
+#                 "info": "italic",
+#                 "warning": "bold",
+#                 "danger": "reverse bold",
+#             },
+#         ),
+#     ]
+
+#     # test = os.path.expanduser(".")
+#     # pass
+
+#     console = Console(theme=_theme)
+#     # console.print("[info] this is a hugo test [warning] WARNING")
+#     console.print("this is a hugo test WARNING",style="info")
+
+
+#     # print("\n")
+
+#     # console.print("This is information", style="info")
+#     # console.print("[warning]The pod bay doors are locked[/warning]")
+#     # console.print("Something terrible happened!", style="danger")
+
+#     # pass
+
+
+
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s:[%(name)s.%(funcName)s(%(lineno)d)]: %(message)s',
                         level=LOG_LEVEL, datefmt="%Y-%m-%d %H:%M:%S",
                         handlers=[RichHandler(rich_tracebacks=True)])
-    # show themes 
-    ColorMapper().show_themes()
+
+    # # show color themes
+    if False:
+        ColorMapper().show_themes()
     # show color maps
-    ColorMapper().show_colors()
-    ColorMapper().show_colors(colors=["red","pink","coral","salmon","orange","yellow","gold","green","cyan","turq","blue","violet","purple","gray"])
-    ColorMapper().show_colors(colors=["dark","medium","light","pale"])
+    if False:
+        ColorMapper().show_colors(num_colums=4)
+        ColorMapper().show_colors(colors=["red","pink","coral","salmon","orange","yellow","gold","green","cyan","turq","blue","violet","purple","gray"])
+        ColorMapper().show_colors(colors=["dark","medium","light","pale"])
+    # handling via console
+    if True:
+        theme_console = ThemeConsole(create_themes=True)
+        # theme_console._theme_manager.get()
+        theme_console.list_themes()
+        # theme_console.preview_theme("ubuntu")
+        theme_console.preview_themes()
+
+    # test_theme()
