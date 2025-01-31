4fc1a782bd1c587f82df8bed957fb034711defc3
hfessler <49552433+aiventures@users.noreply.github.com>
2025-01-31 23:54:33 +0100

    Adding Color Scheme

--- src/model/model_visualizer.py
+++ src/model/model_visualizer.py
@@ -8,6 +8,62 @@ from pydantic import BaseModel
 # pencolor bounding box color
 # x11 https://graphviz.org/doc/info/colors.html#x11
 
+# TODO PRIO2 Use Color Schemes to render Trees according to depth level
+# Prompts
+# graphviz python color scheme
+# graphviz set font color according to color scheme automatically
+# https://graphviz.org/doc/info/colors.html#brewer
+# g = graphviz.Graph()
+# g.attr(colorscheme='set38')  # Use the 'set38' color scheme
+# g.node('A', color='1', fontcolor='1')
+# g.edge('A', 'B', color='3')
+
+# accent8 , blue9, brown blue green11, greens9, bluepurple9,dark2 8,greenblue 9,greens 9,grey9,
+# oranges 9, orange red 9,paired 12, pastel1 9, pastel2 8,
+# pink yellow green 11, purple green 11, purple blue 9,
+# purple blue green 9,purple orange 11,purple red 1,
+# purple 9, red blue 11,red grey 9, red purple 9 ,
+# red yellow blue 11, red yellow green 11, red 9,
+# set1 9, set2 8, set3 12, spectral 11, yellow green 9,
+# yellow green blue 9, yello orange brown 9, yellow orange red 9
+
+GraphVizColorScheme = Literal[
+    "accent",
+    "brbg",
+    "blues",
+    "bugn",
+    "bupu",
+    "dark2",
+    "gnbu",
+    "greens",
+    "greys",
+    "oranges",
+    "orrd" "paired",
+    "pastel1",
+    "pastel2",
+    "piyg",
+    "prgn",
+    "pubu",
+    "pubugn" "puor",
+    "purd",
+    "purple",
+    "rdbu",
+    "rdgy",
+    "rdpu",
+    "rdylbu",
+    "rdylgn",
+    "reds",
+    "set1",
+    "set2",
+    "set3",
+    "spectral",
+    "ylgn",
+    "ylgnbu",
+    "ylorbr",
+    "ylorrd",
+]
+GraphVizColorSchemeType = Annotated[GraphVizColorScheme, "color_scheme"]
+
 # shapes Attribute
 # https://graphviz.org/doc/info/shapes.html
 # record "a|b|c"
